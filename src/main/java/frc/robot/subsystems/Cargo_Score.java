5a90b886c97d89e3db3397b04719c3f1545758cd
slynch05 <slynch05@yahoo.com>
2019-01-09 21:25:32 -0500

    added functions for cargo score.

--- src/main/java/frc/robot/subsystems/Cargo_Score.java
+++ src/main/java/frc/robot/subsystems/Cargo_Score.java
@@ -8,18 +8,22 @@
 package frc.robot.subsystems;
 
 import frc.robot.commands.*;
-
+import edu.wpi.first.wpilibj.Solenoid;
+import edu.wpi.first.wpilibj.I2C.Port;
 import edu.wpi.first.wpilibj.command.Subsystem;
 
 /**
- * 
+ * @author Zayd A.
+ * It allows you to open and close claw
  */
 public class Cargo_Score extends Subsystem {
+public Solenoid  claw;
 
-
+private int PCM0 = 0;
+private int Port = 0;
 
   public Cargo_Score() {
-
+    this.claw = new Solenoid(PCM0, Port);
   }
 
   // Set the default command for a subsystem here.
@@ -27,6 +31,20 @@ public class Cargo_Score extends Subsystem {
   @Override
   public void initDefaultCommand() {
     setDefaultCommand(new Example_Command());
+
   }
 
+  /**
+   * clawOpen = Opens claw; sets this to true
+   */
+  public void clawOpen() {
+    claw.set(true);
+  }
+
+  /**
+   * clawClose = Closes claw; sets this to false
+   */
+  public void clawClose() {
+    claw.set(false);
+  }
 }
