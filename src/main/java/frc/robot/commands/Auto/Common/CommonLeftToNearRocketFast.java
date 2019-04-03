ab5219b525bfc2187ceba72416e7eed81fc88687
brycegerman <r2bryce2@gmail.com>
2019-04-03 14:05:27 -0400

    Made fast common drive files

---
+++ src/main/java/frc/robot/commands/Auto/Common/CommonLeftToNearRocketFast.java
@@ -0,0 +1,34 @@
+package frc.robot.commands.Auto.Common;
+
+import frc.robot.commands.Auto.Common.*;
+import frc.robot.commands.HatchBeak.hatchBeakClose;
+
+import com.revrobotics.CANSparkMax.IdleMode;
+
+import edu.wpi.first.wpilibj.command.CommandGroup;
+import frc.robot.Robot;
+import frc.robot.commands.Auto.*;
+
+public class CommonLeftToNearRocketFast extends CommandGroup {
+    public CommonLeftToNearRocketFast() {
+
+    addSequential(new autoTurnOnLimeLightLED());
+    addParallel(new autoLiftToPositionWithWait(Robot.Lift.hatchLowScorePosition, 0.5));
+    addSequential(new autoPIDVisionDrive(3.2, 0.07, 0.02, 0.6));
+    addSequential(new autoResetEncoders());
+    addSequential(new CommonScoreHatch());
+    
+    addParallel(new autoLiftToPositionWithWait(Robot.Lift.hatchLowScorePosition, 0.5));
+    addSequential(new autoTankDrive(-0.6, -0.1, -23000, 0, "left", IdleMode.kCoast)); 
+    addSequential(new autoTurnOnLimeLightLED());
+    addSequential(new autoResetEncoders());
+    addSequential(new autoTankDrive(0.1, 0.6, 0, 16000, "rightVision", IdleMode.kCoast)); 
+    // addSequential(new autoResetSensors());
+    addParallel(new hatchBeakClose());
+    addSequential(new autoPIDVisionDrive(4, 0.09, 0.06, 0.7)); //changed speed to 70 from 60
+    addSequential(new CommonIntakeHatch());
+    addSequential(new autoResetEncoders()); 
+    addSequential(new autoWait(0.05));
+
+    }
+}
