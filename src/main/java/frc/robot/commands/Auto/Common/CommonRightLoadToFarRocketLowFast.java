c353d520a367fae16b81b38e807859a1638d361b
brycegerman <r2bryce2@gmail.com>
2019-04-02 22:00:42 -0400

    Tuned around the rocket drive - could possibly be off

--- src/main/java/frc/robot/commands/Auto/Common/CommonRightLoadToFarRocketLowFast.java
+++ src/main/java/frc/robot/commands/Auto/Common/CommonRightLoadToFarRocketLowFast.java
@@ -23,9 +23,10 @@ public class CommonRightLoadToFarRocketLowFast extends CommandGroup {
     addParallel(new autoLiftToPositionWithWait(Robot.Lift.hatchLowScorePosition + 15, 0.5));
     addSequential(new autoTankDrive(-0.8, -0.64, 60000, 0, "left", IdleMode.kCoast));
     addSequential(new autoTankDrive(-0.64, -0.85, 0, 108000, "right", IdleMode.kCoast));
+    addParallel(new autoTurnOnLimeLightLED());
     addSequential(new autoTankDrive(-0.51, -0.65, 0, 155000, "right", IdleMode.kCoast));
     addSequential(new autoTankDrive(-0.27, 0.32, 177500, 0, "left", IdleMode.kBrake));
     // addSequential(new autoPIDVisionDrive(4, 0.08, 0.06, 0.6));
-    addSequential(new CommonScoreHatch());
+    // addSequential(new CommonScoreHatch());
   }
 }
